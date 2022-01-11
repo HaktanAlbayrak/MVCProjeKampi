d7a74d11896c8a6e9fdc400f5a99b33a4d27dc76
HaktanAlbayrak <haktan.albayrak01@gmail.com>
2022-01-11 18:56:16 +0300

    11/01/2022 18.50 Changes

--- MVCProjeKampi/Controllers/İstatistikController.cs
+++ MVCProjeKampi/Controllers/İstatistikController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,26 @@ namespace MVCProjeKampi.Controllers
     public class İstatistikController : Controller
     {
         // GET: İstatistik
+        Context c = new Context();
         public ActionResult Index()
         {
+            ViewBag.CategoryCount = c.Categories.Count();
+
+
+            ViewBag.CategoryTrue = c.Categories.Where(x => x.CategoryStatus == true).Count();
+            ViewBag.CategoryFalse = c.Categories.Where(x => x.CategoryStatus == false).Count();
+            ViewBag.CategoryStatusCount = ViewBag.CategoryTrue - ViewBag.CategoryFalse;
+
+
+            ViewBag.CategorySoftware = c.Headings.Where(x => x.CategoryID == 25).Count();
+
+
+            ViewBag.WritersName = c.Writers.Where(x => x.WriterName.Contains("A")).Count();
+
+
+            var MostTitles = c.Headings.Where(u => u.CategoryID == c.Headings.GroupBy(x => x.CategoryID).OrderByDescending(x => x.Count())
+                 .Select(x => x.Key).FirstOrDefault()).Select(x => x.Category.CategoryName).FirstOrDefault();
+            ViewBag.MostTitles = MostTitles;
             return View();
         }
     }
